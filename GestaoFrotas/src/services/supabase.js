e8e762033eadf8504a71ec3468eac685eb11ea93
mateus santos motta <mateusmotta7555@gmail.com>
2025-11-27 11:12:06 -0300

    Update 1.7

--- GestaoFrotas/src/services/supabase.js
+++ GestaoFrotas/src/services/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = ""
 const SUPABASE_KEY = ""
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_URL)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
