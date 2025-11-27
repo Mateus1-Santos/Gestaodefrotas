6573326a0f30886d60b89a5942ea3c7cf3bc719c
Felipe <fm3343190@gmail.com>
2025-11-27 10:14:00 -0300

    COMIT

--- GestaoFrotas/App.js
+++ GestaoFrotas/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>meu mengão, flamengo pra sempre e morre gremio</Text>
       <StatusBar style="auto" />
     </View>
   );
